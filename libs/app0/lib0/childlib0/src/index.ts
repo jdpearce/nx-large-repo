7dc48f2ec24c8f5c6ceff40da7be8421579684cb
Jo Hanna Pearce <jo@nrwl.io>
2022-05-31 11:35:42 +0100

    chore: additional change #1

--- libs/app0/lib0/childlib0/src/index.ts
+++ libs/app0/lib0/childlib0/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/childlib0.module';
 export * from './lib/lib0childlib0parent/lib0childlib0parent.component';
 
 // just another smol change to trigger DTE
+
+// additional change #1
